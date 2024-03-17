3a061b8b69c28aad2d47a66bbf6623e6a082475d
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-03-17 08:44:50 -0300

    install tailwind css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gestor de Briefins</h1>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+};
+
+export default App;
