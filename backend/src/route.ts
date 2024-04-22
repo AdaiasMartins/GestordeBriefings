f8ba1a724febca4bed19e27a37bf8d3dc1a63a5c
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2024-04-21 21:29:51 -0300

    bug fix

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -1,35 +1,23 @@
-import fastify, {FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply} from "fastify";
+import fastify, { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 import { CreateBriefinController } from "./Controller/CreateBriefinController";
 import { ListBriefinsController } from "./Controller/ListBriefinsController";
 import { DeleteBriefinController } from "./Controller/DeleteBriefinController";
 import { UpdateBriefinController } from "./Controller/UpdateBriefinController";
 
-
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions) {
-    
+    fastify.post("/briefin", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new CreateBriefinController().handle(request, reply);
+    });
 
-    fastify.post("/briefin", async (resquest: FastifyRequest, reply: FastifyReply) =>{
-        return new CreateBriefinController().handle(resquest, reply);
-    })
+    fastify.get("/briefins", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListBriefinsController().handle(request, reply);
+    });
 
-    fastify.get("/briefins", async (resquest: FastifyRequest, reply: FastifyReply) =>{
-        return new ListBriefinsController().handle(resquest, reply);
-    })
-
-    fastify.delete("/briefin/:id", async (request, reply) => {
+    fastify.delete("/briefin/:id", async (request: FastifyRequest, reply: FastifyReply) => {
         return new DeleteBriefinController().handle(request, reply);
     });
 
-    fastify.patch("/briefin/:id", async (request, reply) => {
+    fastify.patch("/briefin/:id", async (request: FastifyRequest, reply: FastifyReply) => {
         return new UpdateBriefinController().handle(request, reply);
-  });
-
-
-
-    
-
-
-    
-
-    
+    });
 }
